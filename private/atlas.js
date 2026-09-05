35418b4e832f635b13d7205bd94ec320e163387f
my first code <myselfsanju666@gmail.com>
2026-09-05 20:06:45 +0530

    after solved

--- private/atlas.js
+++ private/atlas.js
@@ -1,29 +1,11 @@
+import 'dotenv/config';
 import mongoose from 'mongoose'
 
+await mongoose.connect(process.env.MONGO_URI)
 
+const validation = new mongoose.Schema({
+    email: String,
+    otp: String
+})
 
-
-    await mongoose.connect(
-        'mongodb+srv://samson66:samson7890@cluster0.2kgdexp.mongodb.net/otp?appName=Cluster0'
-    )
-
-    const validation = new mongoose.Schema({
-        email: String,
-        otp: String
-    })
-
-   export const db_otp = mongoose.model(
-        'otpverify',
-        validation,
-        'email'
-    )
-
-   
-
-
-
-
-    
-
-
-
+export const db_otp = mongoose.model('otpverify', validation, 'email')
